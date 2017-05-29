901d45afc7aa1463e3a840639a86acd9089c53bb
bing <ybingcheng@gmail.com>
2017-05-29 16:08:49 +0800

    First working type
    Read success

--- pyread.py
+++ pyread.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python
 
 import serial
-import time
+from time import sleep
 
 # Open serial port
 ser = serial.Serial(
-    # /dev/ttyS1
-    # /dev/ttyUSB0
-    port = '/dev/ttyUSB0',
-    # baudrate: 9600, 14400, 19200, 57600, 115200
-    baudrate = 9600,
-    # parity: PARITY_ODD, PARITY_EVEN, PARITY_NONE
-    parity = serial.PARITY_NONE,
-    stopbits = serial.STOPBITS_ONE,
-    bytesize = serial.EIGHGTBITS,
-    # Timeout values:
-    # 1. None: wait forever, block call
-    # 2. 0: Non-blocking mode, return immediately
-    # 3. x, x is bigger than 0, float allowed, timeout blockcall
-    timeout = 1 ,
-    # Disable - False
-    # Software Flow Control
-    xonxoff = False,
-    # Hardware Flow Control, RTS/CTS
-    rtscts = False,
-    # Hardware Flow Control, DSR/DTR
-    dsrdtr = False,
-    # Timeout for write
-    writeTimeout = 2
+    port="/dev/ttyUSB0",  # Set serial port
+    baudrate=9600,  # baudrate: 9600, 14400, 19200, 57600, 115200
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=0,
+    xonxoff=False,
+    rtscts=False,
+    dsrdtr=False,
+    write_timeout=1
 )
 
-try:
-    ser.open()
-except Exception, e:
-    print "error opening serial port: " + str(e)
-    exit()
-
-if ser.isOpen()
-    try:
-        ser.flushInput() #flush input buffer, discarding all contents
-        ser.flushOutput() #flush output buffer, abort and discard current output
-
-        
+counter = 32  # Below 32 everything in ASCII is gibberish
+while True:
+    counter += 1
+    ser.write(str(chr(counter)))
+    print ser.readline()
+    sleep(.1)
+    if counter == 255:
+        counter = 32
