50dc2eb8873bfd1d6656647a4a527b36ae5bc8b1
bing <ybingcheng@gmail.com>
2017-05-31 18:24:34 +0800

    End of work commit
    TBD: remove counter for pywrite

--- pywrite.py
+++ pywrite.py
@@ -6,23 +6,17 @@ pywrite.
 ~~~~~~~~~~
 This module writes to serial using python.
 """
+from openport import open_port
+from time import sleep
+import sys
 
-import serial
+ser = open_port(None)
 
-config_file = open("portconfig.txt")
-cport = config_file.readline().rstrip('\n')
-cbaudrate = config_file.readline()
-
-# Open serial port
-ser = serial.Serial(
-    port=cport,  # Set serial port
-    baudrate=cbaudrate,  # baudrate: 9600, 14400, 19200, 57600, 115200
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    timeout=0,
-    xonxoff=False,
-    rtscts=False,
-    dsrdtr=False,
-    write_timeout=0
-)
+cmd = raw_input("Send Command? >> ")
+print "You have send " + cmd + " to serial port"
+if cmd is not None:
+    ser.write(cmd)
+    sleep(.1)
+else:
+    print "Exiting"
+    sys.exit()
