d60e119c63db1238c0ba292d8ec9cffb2121c2c9
bing <ybingcheng@gmail.com>
2017-06-02 14:56:04 +0800

    Open close port
    Object created

--- pyintf.py
+++ pyintf.py
@@ -14,6 +14,8 @@ import os
 import serial
 import threading
 
+# STX = 0x02
+# ENQ = SNDA<CR>
 
 class Serialport(object):
     """Create a serial port class."""
@@ -23,6 +25,41 @@ class Serialport(object):
         """Return port, baudrate."""
         self.serial = serial.Serial(port, baudrate, bytesize, parity,
                                     stopbits, timeout)
+        self.in_cmd = 0
+
+    def start(self):
+        """Start serial port thread."""
+        self.alive = True
+        self.receiver_thread = threading.Thread(target=self.reader)
+        self.receiver_thread.setDaemon(True)
+        self.receiver_thread.start()
+
+    def stop(self):
+        """Close serial port."""
+        self.serial.close()
+        self.alive = False
+
+    def join(self):
+        """Join thread."""
+        self.receiver_thread.join()
+
+    def reader(self):
+        """Read serial port."""
+#        try:
+#            while self.alive:
+#                data = self.serial.read(1)
+#                self.buffer += data
+#                rxlen = len(self.buffer)
+#                if (rxlen == 1 and self.buffer[0] == chr(STX)
+#                   and self.in_cmd == 1):
+#                    print "Message received"
+#                    self.serial.write(chr(ENQ))
+#                    self.in_cmd = 0
+#                    self.buffer = ser.reset_input_buffer
+#                sys.stdout.flush()
+#        except serial.SerialException, e:
+#            self.alive = False
+#            raise
 
 
 def main(argv):
@@ -40,4 +77,7 @@ def main(argv):
         sys.stderr.write("Port cannot be open %r: %s\n" % (port, e))
         sys.exit(1)
 
-    sys.stderr.write()
+    sys.stderr.write(' Reading port on %s : %d\n' % (
+        sp.serial.port,
+        sp.serial.baudrate
+    ))
